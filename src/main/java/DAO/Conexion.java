7da6bd8f596328e78e2452a09ebc6481a649ce61
JUNMPI <150504874+JUNMPI@users.noreply.github.com>
2024-11-13 11:41:28 -0500

    Update Conexion.java

--- src/main/java/DAO/Conexion.java
+++ src/main/java/DAO/Conexion.java
@@ -9,70 +9,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-   private String ruta;
    private Connection con;
-   private String user;
-   private String password;
 
-   public String getRuta() {
-       return ruta;
-   }
+    public Conexion() {
+        this.con = null;
+    }
 
-   public void setRuta(String ruta) {
-       this.ruta = ruta;
-   }
+    public Connection getConnection() {
+        Connection cn = null;
+        try {
+            String dbHost = System.getenv("DB_HOST");
+            String dbPort = System.getenv("DB_PORT");
+            String dbName = System.getenv("DB_NAME");
+            String dbUser = System.getenv("DB_USER");
+            String dbPassword = System.getenv("DB_PASSWORD");
+            String url = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName;
 
-   public Connection getCon() {
-       return con;
-   }
-
-   public void setCon(Connection con) {
-       this.con = con;
-   }
-
-   public String getUser() {
-       return user;
-   }
-
-   public void setUser(String user) {
-       this.user = user;
-   }
-
-   public String getPassword() {
-       return password;
-   }
-
-   public void setPassword(String password) {
-       this.password = password;
-   }
-
-   public Conexion() {
-       this.con = null;
-       this.ruta = "jdbc:mysql://junction.proxy.rlwy.net:47515/railway";
-       this.user = "root";
-       this.password = "DXRdDJsduVBJuofZIqcVXADxqizKFNUL";
-   }
-
-   public Conexion(String ruta, Connection con, String user, String password) {
-       this.ruta = ruta;
-       this.con = con;
-       this.user = user;
-       this.password = password;
-   }
-    
-   public Connection getConnection() {
-       Connection cn = null;
-       try {
-           Class.forName("com.mysql.cj.jdbc.Driver");
-           cn = DriverManager.getConnection(this.ruta, this.user, this.password);
-       } catch (ClassNotFoundException e) {
-           System.out.println("Error: No se encontró el controlador JDBC.");
-           e.printStackTrace();
-       } catch (SQLException e) {
-           System.out.println("Error: No se pudo establecer la conexión con la base de datos.");
-           e.printStackTrace();
-       }
-       return cn;
-   }
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection(url, dbUser, dbPassword);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: No se encontró el controlador JDBC.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error: No se pudo establecer la conexión con la base de datos.");
+            e.printStackTrace();
+        }
+        return cn;
+    }
 }
 
